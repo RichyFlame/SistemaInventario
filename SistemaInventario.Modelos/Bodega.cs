02dd1b8323a01d3ab813447934ef088e8d0134d7
RichyFlame <3094047891210@ingenieria.usac.edu.gt>
2023-08-24 22:17:35 -0600

    Configuracion del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(70, ErrorMessage = "Maximo 70 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es requerido")]
+        [MaxLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
